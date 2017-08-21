7cd2e9715848d5765e5f0b200385db4dd1f7a397
Administrator <Administrator@USER-20170329SH>
2017-08-21 18:02:48 +0800

    新增print（）

--- firstGitTest/src/com/fanzy/TestGitFirst.java
+++ firstGitTest/src/com/fanzy/TestGitFirst.java
@@ -2,12 +2,16 @@ package com.fanzy;
 
 public class TestGitFirst {
 
-int a = 9;
-	
-	public int sum(int x ,int y){
-		return x+y;
+	int a = 9;
+
+	public void print(){
+		
 	}
-	
+
+	public int sum(int x, int y) {
+		return x + y;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		TestGitFirst t = new TestGitFirst();
